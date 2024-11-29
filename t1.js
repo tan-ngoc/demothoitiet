9c72a81494680a83b7d8974ba05ef2e4725303b6
tan-ngoc <nngoctan26@gmail.com>
2024-11-29 17:44:25 +0700

    init project

---
+++ t1.js
@@ -0,0 +1,8 @@
+// Giả lập dữ liệu thời tiết
+document.addEventListener("DOMContentLoaded", function () {
+    const temperature = 30; // Nhiệt độ giả lập
+    const humidity = 65;    // Độ ẩm giả lập
+
+    document.getElementById("temperature").textContent = temperature;
+    document.getElementById("humidity").textContent = humidity;
+});
